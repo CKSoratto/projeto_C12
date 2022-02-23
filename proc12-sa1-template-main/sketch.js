e1818a8b1aee0e6ee073903ee128f6fd6d325e2e
CKSoratto <97638142+CKSoratto@users.noreply.github.com>
2022-02-23 15:00:11 -0300

    Add files via upload

---
+++ proc12-sa1-template-main/sketch.js
@@ -0,0 +1,31 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+for (var a = 0; a<=9; a = a+1){
+
+if (marks[a]>=45){
+  console.log(marks[a])
+} 
+
+
+
+}
+
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+
+  background(30);
+
+
+
+
+}
+
+ 
+
